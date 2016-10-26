053359a047f8a5a71031e247232ba127ce9711f9
PaolaOlvera27 <apao.iq1@gmail.com>
2016-10-26 11:30:13 -0500

    avance

--- bicis-locas-lvl2/js/main.js
+++ bicis-locas-lvl2/js/main.js
@@ -1,7 +1,39 @@
-function validateForm(){
- 	var errores = true;
- 	var email = document.getElementById ("email").value;
- 	var nombre = ("nombre").value;	
- 	var mensaje = " ";
+var  inputs =document.getElementsByClassName ("input-box");
+var i;
+//Funcion que valida cada funcion de los inputs
+function validateForm() {
+		validateName();
+		validateLastename();
+		validateEmail();
+		validatePassword();
+		validateSelect();
+}
+//Funciones de validacion
+function validateName() {
+		var name = document.getElementsById("name");
+		var space = /^\s*$/;
+		var regexname = /^([a-z ñáéíóú]{2,60})$/i;
 
 }
+function validateLastname() {
+		var lastname = document.getElementsById("lastname");
+		var space = /^\s*$/;
+		var regexname = /^([a-z ñáéíóú]{2,60})$/i;
+}
+function validateEmail() {
+		var email = document.getElementsById("input-email");
+		var space = /^\s*$/;
+		var regexemail = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i; // regex de email
+}
+function validatePassword() {
+		var name = document.getElementsById("input-password");
+		var space = /^\s*$/;
+		var regexpassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])\w{6,}$/; // regex para passsword de sólo letras y números sin espacios u otros caracteres
+}
+function validateSelect() {
+		var select = document.getElementsByTagName("select").selectedIndex;
+
+		if( select == null || select == 0 ) {
+		  return false;
+		}
+	}
